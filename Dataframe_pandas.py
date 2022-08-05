625925908f88ffa9b5062427fe573a7e0441ec71
Pooja Vhanzende <poojavhanzende133@gamil.com>
2022-08-05 10:33:41 +0530

    modified in dataframe and create add.txt file

--- Dataframe_pandas.py
+++ Dataframe_pandas.py
@@ -2,3 +2,5 @@
 import pandas as pd
 table = pd.DataFrame({'Name': ['Pooja', 'Pritee', 'Vinayak', 'Vivek'], 'Marks': [89, 87, 90, 88]})
 print(table)
+file = pd.read_csv("country.csv")
+print('hello')
