99e5669a19d64a67ecac3313b2d89bdd1487ff85
Pooja Vhanzende <poojavhanzende133@gamil.com>
2022-08-05 10:05:17 +0530

    dataframe program using pandas lib

---
+++ Dataframe_pandas.py
@@ -0,0 +1,4 @@
+# in this we create multi dimensional data structure i.e. called dataframe
+import pandas as pd
+table = pd.DataFrame({'Name': ['Pooja', 'Pritee', 'Vinayak', 'Vivek'], 'Marks': [89, 87, 90, 88]})
+print(table)
