20dabf4ec4b345c7c29c3a187268960dc6f803ad
Pooja Vhanzende <poojavhanzende133@gamil.com>
2022-08-05 11:53:29 +0530

    correct the error

--- Dataframe_pandas.py
+++ Dataframe_pandas.py
@@ -5,5 +5,5 @@ print(table)
 file = pd.read_csv("country.csv")
 print(file.head())          #it gives th first five data from file
 print(file.tail())          #it gives the last five data
-#print(file.shape())         #it gives the info of rows and columns
+print(file.shape)         #it gives the info of rows and columns
 print(file.describe())      #it gives the info of table
