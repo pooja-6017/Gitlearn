0b089c84346f37c9214c0b5c826191b495af1bac
Pooja Vhanzende <poojavhanzende133@gamil.com>
2022-08-05 10:40:50 +0530

    dataframe modified and created country.csv file

--- Dataframe_pandas.py
+++ Dataframe_pandas.py
@@ -3,4 +3,7 @@ import pandas as pd
 table = pd.DataFrame({'Name': ['Pooja', 'Pritee', 'Vinayak', 'Vivek'], 'Marks': [89, 87, 90, 88]})
 print(table)
 file = pd.read_csv("country.csv")
-print('hello')
+print(file.head())          #it gives th first five data from file
+print(file.tail())          #it gives the last five data
+#print(file.shape())         #it gives the info of rows and columns
+print(file.describe())      #it gives the info of table
